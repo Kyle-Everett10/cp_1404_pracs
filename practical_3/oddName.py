ada39f0edad8ce2c90a9f1e41dac288a5143e631
Kyle Everett <kyle.everett@my.jcu.edu.au>
2018-03-19 09:52:26 +1000

    "Add a name iterator" "Add function to iterate a variable amount of characters"

--- practical_3/oddName.py
+++ practical_3/oddName.py
@@ -1,9 +1,20 @@
 """Kyle"""
-name = input("Enter your name here: ")
-while name == "":
-    print("Not a valid name")
+def main():
     name = input("Enter your name here: ")
-for character in range(0, len(name), 2):
-        print(name[character])
+    while name == "":
+        print("Not a valid name")
+        name = input("Enter your name here: ")
+    frequency = int(input("How frequent do you want the names to be taken?: "))
+    print(extract_characters(name, frequency))
 
+
+def extract_characters(name, frequency):
+    characters = []
+    for character in range(0, len(name), frequency):
+            characters.append(name[character])
+    extracted_characters = " ".join(characters)
+    return extracted_characters
+
+
+main()
 print("Done")
