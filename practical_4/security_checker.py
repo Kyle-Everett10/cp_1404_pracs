b873404470401c237f2d49f01de875eaf3f26852
Kyle Everett <kyle.everett@my.jcu.edu.au>
2018-03-20 11:49:13 +1000

    "Add a new practical folder"

--- practical_4/security_checker.py
+++ practical_4/security_checker.py
@@ -1,1 +1,11 @@
 usernames = ["jimbo", "gilston98", "derekf", "WhatSup"]
+attempted_name = input("Enter your user name: ")
+access_granted = False
+for username in usernames:
+    if attempted_name == username:
+        access_granted = True
+
+if access_granted is True:
+    print("Access Granted")
+else:
+    print("Access denied")
