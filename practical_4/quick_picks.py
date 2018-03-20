ad6642254ef683a7b77f41c8ac1c3ea186ead0ba
Kyle Everett <kyle.everett@my.jcu.edu.au>
2018-03-20 12:59:00 +1000

    "Remove index"

--- practical_4/quick_picks.py
+++ practical_4/quick_picks.py
@@ -4,7 +4,6 @@ number = 0
 for lines in range(0, number_of_lines):
     numbers = []
     list_has_values = False
-    index = 0
     for current_number in range(0, 6):
         new_number = random.randrange(1, 45)
         if list_has_values is True:
@@ -14,13 +13,11 @@ for lines in range(0, number_of_lines):
                     while new_number == number:
                         new_number = random.randrange(1, 45)
                     if new_number > number:
-                        numbers.insert(index, new_number)
+                        numbers.insert(, new_number)
                     elif new_number < number:
-                        numbers.insert(index-2, new_number)
-                    index += 1
+                        numbers.insert(, new_number)
                     break
         else:
             numbers.append(new_number)
             list_has_values = True
-            index += 1
     print(numbers)
