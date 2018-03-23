c0ca5d3703b490785614a06b18afb9a1878e3531
Kyle Everett <kyle.everett@my.jcu.edu.au>
2018-03-23 10:14:42 +1000

    "Remove index" "Add index and an index for the new number" "Add the main and generate lines functions"

--- practical_4/quick_picks.py
+++ practical_4/quick_picks.py
@@ -1,23 +1,37 @@
 import random
-number_of_lines = int(input("Enter how many quick picks you want: "))
-number = 0
-for lines in range(0, number_of_lines):
+
+
+def main():
+    number_of_lines = int(input("Enter how many quick picks you want: "))
+    for lines in range(0, number_of_lines):
+        print(generate_line())
+
+
+def generate_line():
     numbers = []
     list_has_values = False
     for current_number in range(0, 6):
         new_number = random.randrange(1, 45)
         if list_has_values is True:
-            for number in numbers:
-                test = number
-                while len(numbers) != current_number:
-                    while new_number == number:
-                        new_number = random.randrange(1, 45)
-                    if new_number > number:
-                        numbers.insert(, new_number)
-                    elif new_number < number:
-                        numbers.insert(, new_number)
-                    break
+            index = 0
+            new_number_index = 0
+            checked_lines = 0
+            for NUMBER in numbers:
+                while new_number == NUMBER:
+                    new_number = random.randrange(1, 45)
+            while checked_lines != len(numbers):
+                if new_number > numbers[index]:
+                    index += 1
+                    checked_lines += 1
+                    new_number_index += 1
+                elif new_number < numbers[index]:
+                    checked_lines += 1
+                    index += 1
+            numbers.insert(new_number_index, new_number)
         else:
             numbers.append(new_number)
             list_has_values = True
-    print(numbers)
+    return numbers
+
+
+main()
