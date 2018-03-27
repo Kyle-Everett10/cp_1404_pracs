576d37f3a4de38efa4c0b579ea6adcf59042ee37
Kyle Everett <kyle.everett@my.jcu.edu.au>
2018-03-27 11:39:23 +1000

    "Fix the loop type for better syntax"

--- practical_4/number_list.py
+++ practical_4/number_list.py
@@ -1,10 +1,8 @@
-current_input = 0
 MAXIMUM_INPUTS = 5
 numbers = []
-while current_input < MAXIMUM_INPUTS:
+for i in range(0, MAXIMUM_INPUTS):
     number = int(input("Enter a number: "))
     numbers.append(number)
-    current_input += 1
 print("The first number is: {}".format(numbers[0]))
 print("The last number is: {}".format(numbers[-1]))
 print("The smallest number is: {}".format(min(numbers)))
