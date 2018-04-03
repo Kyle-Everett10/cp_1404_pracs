d737dafcc8f55925c5277640f963a4b96c892a63
Kyle Everett <kyle.everett@my.jcu.edu.au>
2018-04-03 12:58:46 +1000

    "Create this new practical" "Add functionality to buttons"

--- practical_6/miles_to_km_converter.py
+++ practical_6/miles_to_km_converter.py
@@ -1,29 +1,26 @@
 from kivy.app import App
 from kivy.lang import Builder
 
+MILES_TO_KILOMETERS_RATE = 1.60934
+
 
 class MilesToKilometersConverter(App):
     def build(self):
-        self.title = "Miles to Kilometers converter"
+        self.title = "Miles to Kilometers converter v1.0"
         self.root = Builder.load_file('converter_layout.kv')
         return self.root
 
     def handle_increment(self, increment):
         try:
             self.root.ids.input_number.text = str(int(self.root.ids.input_number.text) + increment)
-        except TypeError:
-            self.root.ids.input_number.text = str(0+increment)
-        except ValueError:
+        except (TypeError, ValueError):
             self.root.ids.input_number.text = str(0+increment)
 
     def handle_conversion(self, value):
         try:
             local_value = int(value)
-            self.root.ids.output_label.text = str(local_value * 1.60934)
-        except TypeError:
-            local_value = 0
-            self.root.ids.output_label.text = str(local_value)
-        except ValueError:
+            self.root.ids.output_label.text = str(local_value * MILES_TO_KILOMETERS_RATE)
+        except (TypeError, ValueError):
             local_value = 0
             self.root.ids.output_label.text = str(local_value)
 
--- practical_6/squaring.py
+++ practical_6/squaring.py
@@ -17,7 +17,7 @@ class SquareNumberApp(App):
     def build(self):
         """ build the Kivy app from the kv file """
         Window.size = (250, 250)
-        self.title = "Square Number"
+        self.title = "Square Number vu"
         self.root = Builder.load_file('squaring.kv')
         return self.root
 
