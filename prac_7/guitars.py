c5e4bc957afc9c997f3dcc54eecf1e8ce8dd1db7
Kyle Everett <kyle.everett@my.jcu.edu.au>
2018-04-24 12:54:57 +1000

    "Fix the add_guitar function's while loop and used enumerate to get the index"

--- prac_7/guitars.py
+++ prac_7/guitars.py
@@ -1,34 +1,24 @@
 from prac_7.guitar import Guitar
+
 print("MY GUITARS!")
 
 
 def main():
-    index = 1
     guitars = []
     add_guitar(guitars)
-    for guitar in guitars:
-        print("Guitar {}: {} ({:4}), worth ${:6}".format(index, guitar.name, guitar.year, guitar.cost))
-        index += 1
+    for index, guitar in enumerate(guitars):
+        print("Guitar {}: {} ({:4}), worth ${:6}".format(index + 1, guitar.name, guitar.year, guitar.cost))
 
 
-def add_guitar(list):
+def add_guitar(guitars):
     guitar_name = input("Name: ")
-    if guitar_name == "":
-        return list
-    guitar_year = int(input("Year: "))
-    guitar_cost = float(input("Cost: $"))
-    new_guitar = Guitar(guitar_name, guitar_cost, guitar_year)
-    list.append(new_guitar)
-    print("{} ({}), worth ${} added!".format(guitar_name, guitar_year, guitar_cost))
     while guitar_name != "":
-        guitar_name = input("Name: ")
-        if guitar_name == "":
-            return list
         guitar_year = int(input("Year: "))
         guitar_cost = float(input("Cost: $"))
         new_guitar = Guitar(guitar_name, guitar_cost, guitar_year)
-        list.append(new_guitar)
+        guitars.append(new_guitar)
         print("{} ({}), worth ${} added!".format(guitar_name, guitar_year, guitar_cost))
+        guitar_name = input("Name: ")
 
 
 main()
