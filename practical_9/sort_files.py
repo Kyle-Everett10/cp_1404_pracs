5e1697972deb6a3e8086ccf052757cc76867286f
kaeve <kyle.everett@my.jcu.edu.au>
2018-05-15 09:45:24 +1000

    "Split the code into functions"

--- practical_9/sort_files.py
+++ practical_9/sort_files.py
@@ -5,27 +5,35 @@ import os
 def main():
     os.chdir("FilesToSort")
     print(os.getcwd())
-    extension_and_directory = {}
-    directories = []
-    extensions = []
     for dir_name, dir_list, file in os.walk(os.getcwd()):
-        for filename in file:
-            name_and_extension = filename.split(".")
-            print(name_and_extension)
-            if name_and_extension[1] not in extensions:
-                extensions.append(name_and_extension[1])
-        for extension in extensions:
-            directory_for_extension = input("Where do you want {} files?: ".format(extension))
-            extension_and_directory[extension] = directory_for_extension
-            if directory_for_extension not in directories:
-                directories.append(directory_for_extension)
-        for directory in directories:
-            os.mkdir(directory)
-        for filename in file:
-            for extension in extension_and_directory:
-                if extension == filename.split(".")[1]:
-                    shutil.move(filename, extension_and_directory.get(extension))
+        extension_types_and_directory = create_directories(file)
+        move_files(file, extension_types_and_directory)
     print("All sorted! Have a nice day!")
 
+
+def create_directories(file):
+    directories = []
+    extensions = []
+    extension_and_directory = {}
+    for filename in file:
+        name_and_extension = filename.split(".")
+        print(name_and_extension)
+        if name_and_extension[1] not in extensions:
+            extensions.append(name_and_extension[1])
+    for extension in extensions:
+        directory_for_extension = input("Where do you want {} files?: ".format(extension))
+        extension_and_directory[extension] = directory_for_extension
+        if directory_for_extension not in directories:
+            directories.append(directory_for_extension)
+    for directory in directories:
+        os.mkdir(directory)
+    return extension_and_directory
+
+
+def move_files(file, dictionary):
+    for filename in file:
+        for extension in dictionary:
+            if extension == filename.split(".")[1]:
+                shutil.move(filename, dictionary.get(extension))
 
 main()
